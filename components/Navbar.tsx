ea0b9d6e8cfd842190c1b919416fba9b368e07a6
pierre-alexis foy <pierre-alexisdu56@hotmail.fr>
2023-06-27 18:52:56 +0200

    menuBUrger is Ok with ternary to open or close in small screen. Need to style the items and the logo to make them on the same line in md screen

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,12 @@
 "use client"
 import React from 'react'
-// import { useState } from 'react'
-// import { useTheme } from 'next-themes'
+import { useState } from 'react'
+import { useTheme } from 'next-themes'
 import  Image  from "next/image"
 import  Link  from "next/link";
-import { RiMacFill, RiMoonFill, RiSunLine } from "react-icons/ri";
-import { useTheme } from 'next-themes';
+import { RiMoonFill, RiSunLine } from "react-icons/ri";
+import { CgMenuBoxed } from "react-icons/cg"
+import { AiOutlineCloseCircle } from "react-icons/ai"
 
 
 // Types 
@@ -38,6 +39,7 @@ const Navbar = () => {
 
     const { systemTheme, theme, setTheme } = useTheme();
     const currentTheme = theme === "system" ? systemTheme: theme;
+    const [burgerMenu, setBurgerMenu] = useState(false);
 
 
     return (
@@ -45,50 +47,74 @@ const Navbar = () => {
             <div className='mx-3 justify-between md:items-center md:flex'>
                 <div>
                     <div className='flex items-center justify-between py-3'>
-                        <div className='md:w-full md:py-5 md:px-4 md:block pt-2 px-3'>
-                            <Image 
-                                className='rounded-full'
-                                src="/../public/images/logo_friperie.jpg" 
-                                alt=''   
-                                width={110}              
-                                height={110}              
-                            />
+                        <Image 
+                            className='rounded-full'
+                            src="/../public/images/logo_friperie.jpg" 
+                            alt=''   
+                            width={110}              
+                            height={110}              
+                        />
+                    </div>
+
+                    {/*
+                        Here, we hide the menuBurger according on the screen size : 
+                        Under the md screen size we show
+                        If MD size or plus, the BurgerMenu is hidden
+                        We use a ternary expression : burgerMenu is true, we displays a close button 
+                        BurgerMenu is false, it changes to a burgermenu to open with three horinzontal lines                    
+                        */}
+                    <div className='md:hidden'>
+                        <button 
+                            className='p-2'
+                            onClick={() => setBurgerMenu(!burgerMenu)}
+                        >
+                            {burgerMenu ? <AiOutlineCloseCircle size={30}/> : <CgMenuBoxed size={30} /> }
+                        </button>
+                    </div>
+
+                    {/* 
+                        we wrap our items in the div below : 
+                        if the menu burger is true, we display the the items 
+                        if not, we hide the items
+                    */}
+                    <div
+                        className={'flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ' + (burgerMenu ? 'block' : 'hidden')}
+                    >
+                        <div className='items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0'>
+                            {
+                                navItems.map((item, id) => (
+                                    <Link 
+                                        key={id}
+                                        href={item.page}
+                                        className='block lg:inline-block text-neutral-800 dark:text-neutral-400 hover:text-neutral-500 hover:scale-125 dark:hover:text-neutral-100'
+                                    >
+                                        {item.label}
+                                    </Link>
+                                )
+                            )}
+                            {/*
+                                Here, we switch (toggle) the theme onClick on the button
+                                We use a ternary expression : dark is true, we click on the button so
+                                we switch in light theme. If not, dark is false so we are in light theme 
+                            */}
+                            { currentTheme === "dark" ? (
+                                <button 
+                                    onClick={() => setTheme("light")}
+                                    className='bg-slate-100 p-2 rounded-xl'
+                                >
+                                    <RiMoonFill size={25} color='black' />
+                                </button>
+                            )
+                            : (
+                                <button 
+                                    onClick={() => setTheme ("dark")}
+                                    className='bg-slate-100 p-2 rounded-xl'
+                                >
+                                    <RiSunLine size={25} />
+                                </button>
+                            )}
                         </div>
                     </div>
-                </div>
-                <div className='items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0'>
-                    {
-                        navItems.map((item, id) => (
-                            <Link 
-                                key={id}
-                                href={item.page}
-                                className='block lg:inline-block text-neutral-800 dark:text-neutral-400'
-                            >
-                                {item.label}
-                            </Link>
-                        )
-                    )}
-                    {/*
-                        Here, we switch (toggle) the theme onClick on the button
-                        We use a ternary expression : dark is true, we click on the button so
-                        we switch in light theme. If not, dark is false so we are in light theme 
-                    */}
-                    { currentTheme === "dark" ? (
-                        <button 
-                            onClick={() => setTheme("light")}
-                            className='bg-slate-100 p-2 rounded-xl'
-                        >
-                            <RiMoonFill size={25} color='black' />
-                        </button>
-                    )
-                    : (
-                        <button 
-                            onClick={() => setTheme ("dark")}
-                            className='bg-slate-100 p-2 rounded-xl'
-                        >
-                            <RiSunLine size={25} />
-                        </button>
-                    )}
                 </div>
             </div>
         </header>
