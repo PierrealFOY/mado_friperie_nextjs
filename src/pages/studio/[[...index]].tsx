5205d4752d4aae22cdd005cb4b145a3cf401c113
pierre-alexis foy <pierre-alexisdu56@hotmail.fr>
2023-06-30 10:54:51 +0200

    packages npm sanity client to solve build conflict

--- src/pages/studio/[[...index]].tsx
+++ src/pages/studio/[[...index]].tsx
@@ -3,6 +3,9 @@ import { NextStudio } from 'next-sanity/studio'
 import { metadata } from 'next-sanity/studio/metadata'
 import config from '../../../sanity.config'
 
+// rentrer cette commande permet de d'importer next-sanity studio et metadata qui n'étaient pas reconnus
+// npm install next-sanity @sanity/client @portabletext/react @sanity/image-url
+
 export default function StudioPage() {
   return (
     <>
