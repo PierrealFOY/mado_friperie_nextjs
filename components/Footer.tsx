b976e213c92467585686d75b79e531a44ab9e8a2
pierre-alexis foy <pierre-alexisdu56@hotmail.fr>
2023-07-03 13:01:37 +0200

    text Pierre-Alexis FOY is white in dark mode and have an avatar icon redirects to my portfolio.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import { Typography } from "@material-tailwind/react";
 import { FaFacebook } from "react-icons/fa" 
 import { GrInstagram } from "react-icons/gr" 
+import { RxAvatar } from "react-icons/rx" 
 import { SiGmail } from "react-icons/si" 
 import Link from 'next/link'
 
@@ -8,9 +9,20 @@ import Link from 'next/link'
 export default function SimpleFooter() {
     return (
         <footer className="flex w-full flex-row flex-wrap items-center justify-center px-4 gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between">
-            <Typography color="blue-gray" className="font-normal">
-                &copy; 2023 Pierre-Alexis Foy
-            </Typography>
+            <div className="flex flex-row md:space-x-3 space-x-2">
+                <a href="https://pierre-alexis-foy-portfolio.vercel.app/" target="_blank" >
+                    <RxAvatar
+                        size={30}
+                        className="hover:animate-bounce"
+                    />
+                </a>
+                <a 
+                    href="https://pierre-alexis-foy-portfolio.vercel.app/" 
+                    target="_blank"
+                >
+                    Pierre-Alexis FOY
+                </a>
+            </div>
             
             <div className='flex flex-row space-x-3 items-center justify-center mb-2'>
                     <a href="https://www.facebook.com/madofriperie/" target="_blank" >
