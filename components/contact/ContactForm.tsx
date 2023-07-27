4fa606755132600c0cd10b53dea4b37c1f61a0db
pierre-alexis foy <pierre-alexisdu56@hotmail.fr>
2023-07-27 16:44:22 +0200

    création de la fonction handleSubmit avec axios pour gérer l'envoi de données quand formulaire soumis. preventdefault rajouté pour empêcher le rechargement de la page. A tester pour récupérer ces données

--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -3,6 +3,8 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react"
 import { useState } from "react";
+import axios from 'axios';
+
 
 
 // import des modules Material Tailwind avec les éléments et le style du formulaire
@@ -13,16 +15,77 @@ import {
     Button,
     Typography,
 } from "@material-tailwind/react"
+import { error } from "console";
 
 
 
-function ContactForm () {
+const ContactForm = () => {
+
     // Les hooks d'état ci-dessous correspondent aux champs du formulaire et leur soumission
     const [name, setName] = useState("")
     const [firstname, setFirstname] = useState("")
     const [email, setEmail] = useState("")
     const [message, setMessage] = useState("")
     const [submitted, setSubmitted] = useState(false)
+
+    // fonction de soumission du formulaire
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        console.log('Sending')
+        e.preventDefault();
+        let data = {
+            name,
+            firstname,
+            email,
+            message
+        }
+
+    //On appelle l'URL 'api/contact' que l'on viendra récupérer plus tard
+    // axios('api/contact', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Accept': 'application/json, text/plain, */*',
+    //         'Content-Type': 'application/json'
+    //     },
+    //     // body: JSON.stringify(data)
+    // }).then((response) => {
+    //     console.log('Response received')
+    //     // on passe la soumission à true si le status de la requête est à 200 -> OK
+    //     if (response.status === 200) {
+    //         // on affiche un messsage de succès
+    //         console.log('Response succeeded')
+    //         setSubmitted(true)
+    //         setName("")
+    //         setFirstname("")
+    //         setEmail("")
+    //         setMessage("")
+    //     }
+    // })
+    axios({
+        url:'api/contact',
+        method: 'POST',
+        data: {
+            name: 'P-A',
+            firstname: 'P-A',
+            email: 'P-A',
+            message: 'P-A',
+        }
+    }).then((response) => {
+        console.log('Response received')
+        // on passe la soumission à true si le status de la requête est à 200 -> OK
+        if (response.status === 200) {
+            // on affiche un messsage de succès
+            console.log('Response succeeded')
+            setSubmitted(true)
+            setName("")
+            setFirstname("")
+            setEmail("")
+            setMessage("")
+        }
+    }).catch((error) => {
+        console.log("Error while sending the request:", error);
+    })
+    }
+
 
     return (
         <section className="">
@@ -40,7 +103,12 @@ function ContactForm () {
                         <Input size="lg" className="h-auto mb-4 md:mb-0" label="Message" type="textarea" name="message" onChange={(e)=>{setMessage(e.target.value)}} />
                         <Input size="lg" label="Email" type="mail" name="mail" onChange={(e)=>{setEmail(e.target.value)}}/>
                     </div>
-                    <Button className="mt-6" fullWidth>
+                    <Button 
+                        type="submit" 
+                        className="mt-6" 
+                        // On appelle notre fonction handleSubmit au clic 'OnClick' du bouton envoyer
+                        onClick={(e) =>{handleSubmit(e)}}
+                        fullWidth>
                         Envoyer
                     </Button>
                 </form>
