1ce5e8b70d53cc6b8068d728f59cdf7b89dc99e3
pierre-alexis foy <pierre-alexisdu56@hotmail.fr>
2023-07-27 10:01:48 +0200

    image logo ajoutée en favicon avec le titre dans la barre de l'onglet

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
     <main className="mx-auto max-w-7xl px-4 sm:px-6 md:max-w-7lx ">
       <div>
         <Head>
+          <link rel="icon" href="/favicon.ico" sizes="any" className="rounded-full" />
           <title className="text-black"> Friperie Mado </title>
         </Head>
       </div>
